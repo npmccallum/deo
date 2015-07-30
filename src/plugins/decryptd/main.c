5cf3cd4e43f7757e6c2a4c0b552c73b452042b36
Nathaniel McCallum <npmccallum@redhat.com>
2015-07-30 10:49:43 -0400

    Migrate decryptd to use systemd's Accept=true
    The performance of this option isn't great. However, moving all of the
    code to support async IO is rather challenging.

--- src/plugins/decryptd/main.c
+++ src/plugins/decryptd/main.c
@@ -32,159 +32,105 @@
 #include <unistd.h>
 
 static bool
-have_conn(int lfds, int sock, int *fd)
-{
-    struct pollfd pfds[lfds > 0 ? lfds : 1];
-
-    if (lfds > 0) {
-        for (int i = 0; i < lfds; i++) {
-            pfds[i].fd = SD_LISTEN_FDS_START + i;
-            pfds[i].events = POLLIN | POLLPRI | POLLOUT;
-        }
-    } else if (sock >= 0) {
-        pfds[0].fd = sock;
-        pfds[0].events = POLLIN | POLLPRI | POLLOUT;
-    } else
-        return false;
-
-    if (poll(pfds, lfds > 0 ? lfds : 1, -1) <= 0)
-        return false;
-
-    for (int i = 0; i < (lfds > 0 ? lfds : 1); i++) {
-        if ((pfds[i].revents & (POLLERR | POLLHUP | POLLNVAL)) != 0)
-            return false;
-    }
-
-    for (int i = 0; i < (lfds > 0 ? lfds : 1); i++) {
-        if (pfds[i].revents != 0) {
-            *fd = pfds[i].fd;
-            return true;
-        }
-    }
-
-    return false;
-}
-
-static bool
-do_accept(SSL_CTX *ctx, int fd, int *client, BIO **sio)
-{
-    SSL *ssl;
-
-    *client = accept(fd, NULL, NULL);
-    if (*client < 0)
-        return false;
-
-    *sio = BIO_new_ssl(ctx, 0);
-    if (*sio == NULL)
-        return false;
-
-    if (BIO_get_ssl(*sio, &ssl) <= 0)
-        return false;
-
-    if (SSL_set_fd(ssl, *client) <= 0)
-        return false;
-
-    SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
-    return true;
-}
-
-static void
-on_signal(int sig)
-{
-}
-
-
-static bool
 option(char c, const char *arg, const char **misc)
 {
     *misc = arg;
     return true;
 }
 
-static int
-decryptd(int argc, char *argv[])
+static ctx *
+load_ctx(int argc, char *argv[])
 {
-    const char *hp = DEO_SOCKET;
     const char *tlsfile = NULL;
     const char *encfile = NULL;
     const char *decdir = NULL;
+    ctx *ctx;
+
+    if (!deo_getopt(argc, argv, "ht:e:d:", "", NULL, NULL,
+                    option, &tlsfile, option, &encfile, option, &decdir))
+        goto usage;
+
+    if (tlsfile == NULL || encfile == NULL || decdir == NULL)
+        goto usage;
+
+    ctx = ctx_init(tlsfile, encfile, decdir);
+    if (ctx != NULL)
+        return ctx;
+
+usage:
+    fprintf(stderr, "Usage: deo decryptd "
+            "-t <tlsfile> -e <encfile> -d <decdir>\n");
+    return NULL;
+}
+
+static BIO *
+start_tls(SSL_CTX *ctx)
+{
+    AUTO(BIO, sio);
+    SSL *ssl;
+
+    sio = BIO_new_ssl(ctx, 0);
+    if (sio == NULL)
+        return NULL;
+
+    if (BIO_get_ssl(sio, &ssl) <= 0)
+        return NULL;
+
+    if (SSL_set_fd(ssl, SD_LISTEN_FDS_START) <= 0)
+        return NULL;
+
+    SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
+
+    return STEAL(sio);
+}
+
+static int
+decryptd(int argc, char *argv[])
+{
     int ret = EXIT_FAILURE;
+    AUTO(DEO_MSG, in);
+    AUTO(BIO, sio);
     AUTO(ctx, ctx);
-    int lfds = 0;
-    int sock = 0;
 
-    signal(SIGINT, on_signal);
-    signal(SIGQUIT, on_signal);
-    signal(SIGTERM, on_signal);
-    signal(SIGUSR1, on_signal);
-    signal(SIGUSR2, on_signal);
+    ctx = load_ctx(argc, argv);
+    if (ctx == NULL)
+        goto error;
 
-    if (!deo_getopt(argc, argv, "ht:e:d:l:", "", NULL, NULL,
-                       option, &tlsfile, option, &encfile,
-                       option, &decdir, option, &hp)
-        || tlsfile == NULL || encfile == NULL || decdir == NULL
-        || (ctx = ctx_init(tlsfile, encfile, decdir)) == NULL) {
-        ERR_print_errors_fp(stderr);
-        fprintf(stderr, "Usage: deo decryptd "
-                        "[-l <[host:]port>] -t <tlsfile> "
-                        "-e <encfile> -d <decdir>\n");
-        return EXIT_FAILURE;
-    }
+    sio = start_tls(ctx->ctx);
+    if (sio == NULL)
+        goto error;
 
-    lfds = sd_listen_fds(0);
-    if (lfds <= 0) {
-        sock = BIO_get_accept_socket((char *) hp, 0);
-        if (sock < 0) {
-            ERR_print_errors_fp(stderr);
-            goto error;
-        }
+    in = d2i_bio_max(&DEO_MSG_it, sio, NULL, DEO_MAX_INPUT);
+    if (in == NULL)
+        goto error;
 
-        if (listen(sock, 64) != 0)
-            goto error;
-    }
+    switch (in->type) {
+    case DEO_MSG_TYPE_CRT_REQ:
+        ASN1_item_i2d_bio(&DEO_MSG_it, sio, &(DEO_MSG) {
+            .type = DEO_MSG_TYPE_CRT_REP,
+            .value.crt_rep = ctx->crt
+        });
+        break;
 
-    while (true) {
+    case DEO_MSG_TYPE_DEC_REQ: {
         DEO_ERR err = DEO_ERR_NONE;
         AUTO(ASN1_OCTET_STRING, pt);
-        AUTO(DEO_MSG, in);
-        AUTO(BIO, sio);
-        AUTO_FD(cfd);
-        int lfd;
 
-        if (!have_conn(lfds, sock, &lfd))
-            break;
-
-        if (!do_accept(ctx->ctx, lfd, &cfd, &sio))
-            continue;
-
-        in = d2i_bio_max(&DEO_MSG_it, sio, NULL, DEO_MAX_INPUT);
-        if (in == NULL)
-            continue;
-
-        switch (in->type) {
-        case DEO_MSG_TYPE_CRT_REQ:
-            ASN1_item_i2d_bio(&DEO_MSG_it, sio, &(DEO_MSG) {
-                .type = DEO_MSG_TYPE_CRT_REP,
-                .value.crt_rep = ctx->crt
-            });
-            break;
-
-        case DEO_MSG_TYPE_DEC_REQ:
-            err = decrypt(ctx, in->value.dec_req, &pt);
-            if (err != DEO_ERR_NONE) {
-                SEND_ERR(sio, err);
-                break;
-            }
-
-            ASN1_item_i2d_bio(&DEO_MSG_it, sio, &(DEO_MSG) {
-                .type = DEO_MSG_TYPE_DEC_REP,
-                .value.dec_rep = pt
-            });
-            break;
-
-        default:
+        err = decrypt(ctx, in->value.dec_req, &pt);
+        if (err != DEO_ERR_NONE) {
+            SEND_ERR(sio, err);
             break;
         }
+
+        ASN1_item_i2d_bio(&DEO_MSG_it, sio, &(DEO_MSG) {
+            .type = DEO_MSG_TYPE_DEC_REP,
+            .value.dec_rep = pt
+        });
+        break;
+    }
+
+    default:
+        break;
     }
 
     ret = EXIT_SUCCESS;
